0db7a32c2f6edac5459355b8b4f44cfeeab2c0f7
Jeremy Mesinas <qcjdrmesinas@tip.edu.ph>
2025-05-08 04:51:22 +0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get update && apt-get install -y libgl1 libgtk2.0-dev
-pip install --upgrade pip
+apt-get update && apt-get install -y libgl1
+pip install --upgrade "tensorflow<2.16"  # Force compatible version
 pip install -r requirements.txt
